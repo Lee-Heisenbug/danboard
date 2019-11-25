3172b7a69172c4d30e1386f7063007d138d6ccfa
Lee-Heisenbug <1070068427@qq.com>
2019-11-25 23:00:15 +0800

    Create SceneRenderers/index.js.

---
+++ src/SceneRenderers/index.js
@@ -0,0 +1,4 @@
+export { default as SceneNormalRenderer } from './SceneNormalRenderer';
+export { default as SceneDepthRenderer } from './SceneDepthRenderer';
+export { default as SceneRegularRenderer } from './SceneRegularRenderer';
+export { default as SceneRenderer } from './SceneRenderer';
