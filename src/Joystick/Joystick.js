2af445342ce427b3d03fc3ffeef1e47d6ba55df6
Lee-Heisenbug <1070068427@qq.com>
2020-05-10 23:29:06 +0800

    新增Joystick。

---
+++ src/Joystick/Joystick.js
@@ -0,0 +1,21 @@
+import JoystickObserver from './JoystickObserver';
+
+export default class Joystick {
+    constructor() {
+        /**@type { JoystickObserver } */
+        this.observer = null
+    }
+    /**
+     * @param { number } x 
+     * @param { number } y 
+     */
+    setValue( x, y ) {
+        this.observer.update( x, y );
+    }
+    /**
+     * @param { JoystickObserver } observer 
+     */
+    setObserver( observer ) {
+        this.observer = observer;
+    }
+};
---
+++ src/Joystick/JoystickObserver.js
@@ -0,0 +1,7 @@
+export default class JoystickObserver {
+    /**
+     * @param { number } x 
+     * @param { number } y 
+     */
+    update( x, y ) {}
+};
